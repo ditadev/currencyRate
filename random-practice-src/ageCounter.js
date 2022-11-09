048b0f7de8aedb1d090f915b8a21218004a210ef
InyakaAbraham <abrahaminyaka@yahoo.com>
2022-11-09 13:29:04 +0100

    Updated

--- random-practice-src/ageCounter.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')();
-
-let age = prompt("how old are you? ");
-
-if(age<18){
-    console.log("you are not qualified for NYSC");
-}else{
-    console.log("You ay proceed to NYSC");
-};
